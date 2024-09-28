5ac082771284652e83b05c0297b5e69c7faf61ec
vvhh2002 <drjone@gmail.com>
2024-09-28 16:43:03 +0800

    Update Package.swift
    for iOS 12

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "FreemiumKit",
     defaultLocalization: "en",
-    platforms: [.iOS(.v15), .macOS(.v12), .tvOS(.v15), .visionOS(.v1)],
+    platforms: [.iOS(.v12), .macOS(.v12), .tvOS(.v15), .visionOS(.v1)],
     products: [.library(name: "FreemiumKit", targets: ["FreemiumKit"])],
     targets: [
       .target(name: "FreemiumKit", dependencies: ["FreemiumKitSDK"], resources: [.process("PrivacyInfo.xcprivacy")]),
